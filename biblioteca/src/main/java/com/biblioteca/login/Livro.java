c99365d9ecb192db9f82e3ed48c75088fb169bd0
ericsilva0309 <ericcarvalhosilva03@gmail.com>
2024-09-14 18:24:01 -0300

    Primeira versao biblioteca

---
+++ biblioteca/src/main/java/com/biblioteca/login/Livro.java
@@ -0,0 +1,59 @@
+package com.biblioteca.login;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Livro {
+    private String titulo;
+    private String autor;
+    private String categoria;
+    private int ano;
+    private boolean disponivel;
+
+    public Livro(String titulo, String autor, String categoria, int ano) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.categoria = categoria;
+        this.ano = ano;
+        this.disponivel = true;  // Por padrão, o livro começa como disponível.
+    }
+
+    // Getters e Setters
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    // Método para exibir informações do livro
+    public void exibirDetalhes() {
+        System.out.println("Título: " + titulo);
+        System.out.println("Autor: " + autor);
+        System.out.println("Categoria: " + categoria);
+        System.out.println("Ano: " + ano);
+        System.out.println("Disponível: " + (disponivel ? "Sim" : "Não"));
+    }
+    @Override
+    public String toString() {
+        return "Título: " + titulo + ", Autor: " + autor + ", Categoria: " + categoria + ", Ano: " + ano;
+    }
+}
